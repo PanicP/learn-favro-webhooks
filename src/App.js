5c937e473a6d8297fd73910ebdbe9dcc0abfbd53
PanicP <filmous2136@gmail.com>
2021-02-24 16:09:15 +0700

    WIP: Test call webhooks on web

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import "./App.css";
 
-function App() {
+const App = () => {
+  const fireWebhooks = async () => {
+    let config = {
+      url:
+        "https://services.favro.com/webhook/automation/54d6e99af4b691ae9a79f60a",
+      method: "POST",
+      headers: {
+        "x-token": "82c65b563b8a9cfc1381feec",
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "GET, POST, PATCH, DELETE",
+        "Access-Control-Allow-Headers": "Origin, X-Requested-With, Content-Type, Accept",
+      },
+    };
+    // axios(config)
+    //   .then((response) => { console.log(response)});
+
+    const res = await axios(config);
+
+    // const res = await axios.post(
+    //   "https://services.favro.com/webhook/automation/54d6e99af4b691ae9a79f60a",
+    //   {
+    //     headers: {
+    //       Authorization: "Bearer 82c65b563b8a9cfc1381feec",
+    //       "Content-Type": "application/json",
+    //     },
+    //   }
+    // );
+
+    // const res2 = axios({
+    //   baseURL: "https://services.favro.com/webhook/automation/54d6e99af4b691ae9a79f60a",
+    //   auth: { "x-token": "82c65b563b8a9cfc1381feec" },
+    //   // params: { query: "donkey", page: "1", per_page: "1" },
+    // })
+
+    console.log("res", res);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={fireWebhooks}>fire favro webhooks event</button>
     </div>
   );
-}
+};
 
 export default App;
